77d0c335e9702dbe56a966645ab975b88022852a
maaitlunghau <chunhau.py@gmail.com>
2026-07-22 06:10:18 +0700

    fix: comment out github star button in landing header

--- src/layout/LandingHeader.tsx
+++ src/layout/LandingHeader.tsx
@@ -44,7 +44,7 @@ export function LandingHeader() {
           </div>
 
           <div className="ml-auto flex shrink-0 items-center gap-2">
-            <GithubStarButton />
+            {/* <GithubStarButton /> */}
             <ThemeToggle />
             <Link
               to={`/docs/${firstChapterSlug}`}
